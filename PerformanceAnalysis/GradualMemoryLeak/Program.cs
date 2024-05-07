36049b3135a05810378c52a3e151c76ed46ca6ef
nunkesser <robin.nunkesser@hshl.de>
2024-05-07 10:24:11 +0200

    Memory leaks

---
+++ PerformanceAnalysis/GradualMemoryLeak/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Set the initial memory to allocate (in megabytes)
+
+var memoryToAllocate = 6000; // 6 GB
+// Set the delay between each memory allocation (in milliseconds)
+const int delayBetweenIterations = 1000; // 1 second
+
+while (true)
+{
+    // Allocate memory
+    var memoryChunk =
+        new byte[memoryToAllocate * 1024 * 1024]; // Convert MB to bytes
+
+    // Wait for the specified delay
+    Thread.Sleep(delayBetweenIterations);
+}
---
+++ PerformanceAnalysis/StaticEvents/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+internal static class MemoryLeakExample
+{
+    // Static event (evil!)
+    public static event EventHandler? Evil;
+
+    private static void Main()
+    {
+        while (true)
+            // Subscribe to the event with an anonymous method
+            Evil += delegate { };
+
+        // To avoid the leak, you'd need to unsubscribe from the event:
+        // Evil = null;
+    }
+}
